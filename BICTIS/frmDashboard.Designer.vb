6e55f9cc5e75f105c35373c31512e05e064210fe
Verwin Polloso <verwinpolloso2@gmail.com>
2025-12-09 18:20:12 +0800

    Add project files.

---
+++ BICTIS/frmDashboard.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmDashboard
+    Inherits System.Windows.Forms.Form
+
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    Private components As System.ComponentModel.IContainer
+
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.pnlSidebar = New System.Windows.Forms.Panel()
+        Me.btnResidents = New System.Windows.Forms.Button()
+        Me.btnBlotter = New System.Windows.Forms.Button()
+        Me.btnLogout = New System.Windows.Forms.Button()
+        Me.lblBrand = New System.Windows.Forms.Label()
+        Me.pnlHeader = New System.Windows.Forms.Panel()
+        Me.lblPageTitle = New System.Windows.Forms.Label()
+        Me.pnlContent = New System.Windows.Forms.Panel()
+        Me.PanelKPI1 = New System.Windows.Forms.Panel()
+        Me.lblTotalRes = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PanelKPI2 = New System.Windows.Forms.Panel()
+        Me.lblPendingCases = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        '
+        'pnlSidebar
+        '
+        Me.pnlSidebar.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(45, Byte), Integer))
+        Me.pnlSidebar.Controls.Add(Me.btnLogout)
+        Me.pnlSidebar.Controls.Add(Me.btnBlotter)
+        Me.pnlSidebar.Controls.Add(Me.btnResidents)
+        Me.pnlSidebar.Controls.Add(Me.lblBrand)
+        Me.pnlSidebar.Dock = System.Windows.Forms.DockStyle.Left
+        Me.pnlSidebar.Location = New System.Drawing.Point(0, 0)
+        Me.pnlSidebar.Name = "pnlSidebar"
+        Me.pnlSidebar.Size = New System.Drawing.Size(200, 500)
+        Me.pnlSidebar.TabIndex = 0
+        '
+        'lblBrand
+        '
+        Me.lblBrand.Dock = System.Windows.Forms.DockStyle.Top
+        Me.lblBrand.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.lblBrand.ForeColor = System.Drawing.Color.White
+        Me.lblBrand.Location = New System.Drawing.Point(0, 0)
+        Me.lblBrand.Name = "lblBrand"
+        Me.lblBrand.Size = New System.Drawing.Size(200, 60)
+        Me.lblBrand.TabIndex = 0
+        Me.lblBrand.Text = "TARTARIA BICTIS"
+        Me.lblBrand.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnResidents
+        '
+        Me.btnResidents.Dock = System.Windows.Forms.DockStyle.Top
+        Me.btnResidents.FlatAppearance.BorderSize = 0
+        Me.btnResidents.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnResidents.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.btnResidents.ForeColor = System.Drawing.Color.LightGray
+        Me.btnResidents.Location = New System.Drawing.Point(0, 60)
+        Me.btnResidents.Name = "btnResidents"
+        Me.btnResidents.Size = New System.Drawing.Size(200, 50)
+        Me.btnResidents.TabIndex = 1
+        Me.btnResidents.Text = "Resident Profiling"
+        Me.btnResidents.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnResidents.Padding = New System.Windows.Forms.Padding(15, 0, 0, 0)
+        Me.btnResidents.UseVisualStyleBackColor = True
+        '
+        'btnBlotter
+        '
+        Me.btnBlotter.Dock = System.Windows.Forms.DockStyle.Top
+        Me.btnBlotter.FlatAppearance.BorderSize = 0
+        Me.btnBlotter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnBlotter.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.btnBlotter.ForeColor = System.Drawing.Color.LightGray
+        Me.btnBlotter.Location = New System.Drawing.Point(0, 110)
+        Me.btnBlotter.Name = "btnBlotter"
+        Me.btnBlotter.Size = New System.Drawing.Size(200, 50)
+        Me.btnBlotter.TabIndex = 2
+        Me.btnBlotter.Text = "Blotter / Incidents"
+        Me.btnBlotter.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnBlotter.Padding = New System.Windows.Forms.Padding(15, 0, 0, 0)
+        Me.btnBlotter.UseVisualStyleBackColor = True
+        '
+        'btnLogout
+        '
+        Me.btnLogout.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.btnLogout.FlatAppearance.BorderSize = 0
+        Me.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLogout.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.btnLogout.ForeColor = System.Drawing.Color.Salmon
+        Me.btnLogout.Location = New System.Drawing.Point(0, 450)
+        Me.btnLogout.Name = "btnLogout"
+        Me.btnLogout.Size = New System.Drawing.Size(200, 50)
+        Me.btnLogout.TabIndex = 5
+        Me.btnLogout.Text = "Logout"
+        Me.btnLogout.UseVisualStyleBackColor = True
+        '
+        'pnlHeader
+        '
+        Me.pnlHeader.BackColor = System.Drawing.Color.White
+        Me.pnlHeader.Controls.Add(Me.lblPageTitle)
+        Me.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlHeader.Location = New System.Drawing.Point(200, 0)
+        Me.pnlHeader.Name = "pnlHeader"
+        Me.pnlHeader.Size = New System.Drawing.Size(600, 60)
+        Me.pnlHeader.TabIndex = 1
+        '
+        'lblPageTitle
+        '
+        Me.lblPageTitle.AutoSize = True
+        Me.lblPageTitle.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular)
+        Me.lblPageTitle.Location = New System.Drawing.Point(20, 18)
+        Me.lblPageTitle.Name = "lblPageTitle"
+        Me.lblPageTitle.Size = New System.Drawing.Size(190, 25)
+        Me.lblPageTitle.TabIndex = 0
+        Me.lblPageTitle.Text = "Dashboard Overview"
+        '
+        'pnlContent
+        '
+        Me.pnlContent.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.pnlContent.Controls.Add(Me.PanelKPI2)
+        Me.pnlContent.Controls.Add(Me.PanelKPI1)
+        Me.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlContent.Location = New System.Drawing.Point(200, 60)
+        Me.pnlContent.Name = "pnlContent"
+        Me.pnlContent.Size = New System.Drawing.Size(600, 440)
+        Me.pnlContent.TabIndex = 2
+        '
+        'PanelKPI1
+        '
+        Me.PanelKPI1.BackColor = System.Drawing.Color.White
+        Me.PanelKPI1.Controls.Add(Me.lblTotalRes)
+        Me.PanelKPI1.Controls.Add(Me.Label1)
+        Me.PanelKPI1.Location = New System.Drawing.Point(25, 25)
+        Me.PanelKPI1.Name = "PanelKPI1"
+        Me.PanelKPI1.Size = New System.Drawing.Size(200, 100)
+        Me.PanelKPI1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ForeColor = System.Drawing.Color.Gray
+        Me.Label1.Location = New System.Drawing.Point(10, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Text = "TOTAL RESIDENTS"
+        '
+        'lblTotalRes
+        '
+        Me.lblTotalRes.AutoSize = True
+        Me.lblTotalRes.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold)
+        Me.lblTotalRes.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblTotalRes.Location = New System.Drawing.Point(10, 35)
+        Me.lblTotalRes.Name = "lblTotalRes"
+        Me.lblTotalRes.Text = "0"
+        '
+        'PanelKPI2
+        '
+        Me.PanelKPI2.BackColor = System.Drawing.Color.White
+        Me.PanelKPI2.Controls.Add(Me.lblPendingCases)
+        Me.PanelKPI2.Controls.Add(Me.Label2)
+        Me.PanelKPI2.Location = New System.Drawing.Point(245, 25)
+        Me.PanelKPI2.Name = "PanelKPI2"
+        Me.PanelKPI2.Size = New System.Drawing.Size(200, 100)
+        Me.PanelKPI2.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label2.ForeColor = System.Drawing.Color.Gray
+        Me.Label2.Location = New System.Drawing.Point(10, 10)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Text = "PENDING CASES"
+        '
+        'lblPendingCases
+        '
+        Me.lblPendingCases.AutoSize = True
+        Me.lblPendingCases.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold)
+        Me.lblPendingCases.ForeColor = System.Drawing.Color.Crimson
+        Me.lblPendingCases.Location = New System.Drawing.Point(10, 35)
+        Me.lblPendingCases.Name = "lblPendingCases"
+        Me.lblPendingCases.Text = "0"
+        '
+        'frmDashboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 500)
+        Me.Controls.Add(Me.pnlContent)
+        Me.Controls.Add(Me.pnlHeader)
+        Me.Controls.Add(Me.pnlSidebar)
+        Me.Name = "frmDashboard"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Barangay System Dashboard"
+        Me.pnlSidebar.ResumeLayout(False)
+        Me.pnlHeader.ResumeLayout(False)
+        Me.pnlHeader.PerformLayout()
+        Me.pnlContent.ResumeLayout(False)
+        Me.PanelKPI1.ResumeLayout(False)
+        Me.PanelKPI1.PerformLayout()
+        Me.PanelKPI2.ResumeLayout(False)
+        Me.PanelKPI2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlSidebar As Panel
+    Friend WithEvents btnResidents As Button
+    Friend WithEvents btnBlotter As Button
+    Friend WithEvents btnLogout As Button
+    Friend WithEvents lblBrand As Label
+    Friend WithEvents pnlHeader As Panel
+    Friend WithEvents lblPageTitle As Label
+    Friend WithEvents pnlContent As Panel
+    Friend WithEvents PanelKPI1 As Panel
+    Friend WithEvents lblTotalRes As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PanelKPI2 As Panel
+    Friend WithEvents lblPendingCases As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ BICTIS/frmLogin.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblUser = New System.Windows.Forms.Label()
+        Me.lblPass = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1 (Container for centering)
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.btnCancel)
+        Me.Panel1.Controls.Add(Me.btnLogin)
+        Me.Panel1.Controls.Add(Me.txtPassword)
+        Me.Panel1.Controls.Add(Me.txtUsername)
+        Me.Panel1.Controls.Add(Me.lblPass)
+        Me.Panel1.Controls.Add(Me.lblUser)
+        Me.Panel1.Controls.Add(Me.lblTitle)
+        Me.Panel1.Location = New System.Drawing.Point(40, 40)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(400, 300)
+        Me.Panel1.TabIndex = 0
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(120, Byte), Integer))
+        Me.lblTitle.Location = New System.Drawing.Point(80, 20)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(240, 30)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "BICTIS SECURITY LOGIN"
+        '
+        'lblUser
+        '
+        Me.lblUser.AutoSize = True
+        Me.lblUser.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.lblUser.Location = New System.Drawing.Point(40, 80)
+        Me.lblUser.Name = "lblUser"
+        Me.lblUser.Size = New System.Drawing.Size(74, 19)
+        Me.lblUser.TabIndex = 1
+        Me.lblUser.Text = "Username:"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.txtUsername.Location = New System.Drawing.Point(40, 105)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(320, 25)
+        Me.txtUsername.TabIndex = 1
+        '
+        'lblPass
+        '
+        Me.lblPass.AutoSize = True
+        Me.lblPass.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.lblPass.Location = New System.Drawing.Point(40, 145)
+        Me.lblPass.Name = "lblPass"
+        Me.lblPass.Size = New System.Drawing.Size(70, 19)
+        Me.lblPass.TabIndex = 3
+        Me.lblPass.Text = "Password:"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.txtPassword.Location = New System.Drawing.Point(40, 170)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(320, 25)
+        Me.txtPassword.TabIndex = 2
+        '
+        'btnLogin
+        '
+        Me.btnLogin.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLogin.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.btnLogin.ForeColor = System.Drawing.Color.White
+        Me.btnLogin.Location = New System.Drawing.Point(40, 220)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(150, 40)
+        Me.btnLogin.TabIndex = 3
+        Me.btnLogin.Text = "LOGIN"
+        Me.btnLogin.UseVisualStyleBackColor = False
+        '
+        'btnCancel
+        '
+        Me.btnCancel.BackColor = System.Drawing.Color.FromArgb(CType(CType(220, Byte), Integer), CType(CType(53, Byte), Integer), CType(CType(69, Byte), Integer))
+        Me.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCancel.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.btnCancel.ForeColor = System.Drawing.Color.White
+        Me.btnCancel.Location = New System.Drawing.Point(210, 220)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(150, 40)
+        Me.btnCancel.TabIndex = 4
+        Me.btnCancel.Text = "CANCEL"
+        Me.btnCancel.UseVisualStyleBackColor = False
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(484, 381)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblUser As Label
+    Friend WithEvents lblPass As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnCancel As Button
+End Class
