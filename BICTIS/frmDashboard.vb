6bda3d9951a1746f9fc3387c6f0b5115535292f9
Verwin Polloso <verwinpolloso2@gmail.com>
2025-12-09 18:44:24 +0800

    changed the class to module

--- BICTIS/frmDashboard.vb
+++ BICTIS/frmDashboard.vb
@@ -1,3 +1,51 @@
 ﻿Public Class frmDashboard
+    Dim db As New Session()
 
+    Private Sub frmDashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 1. Load User Info in Title
+        lblPageTitle.Text = "Dashboard Overview - " & Session.CurrentUserRole
+
+        ' 2. Load Statistics
+        LoadDashboardStats()
+    End Sub
+
+    Private Sub LoadDashboardStats()
+        ' KPI 1: Total Residents (Active)
+        Dim countRes As Integer = db.GetCount("SELECT COUNT(*) FROM tbl_Residents WHERE IsActive=True")
+        lblTotalRes.Text = countRes.ToString()
+
+        ' KPI 2: Pending Cases (The Critical Alert)
+        Dim countCases As Integer = db.GetCount("SELECT COUNT(*) FROM tbl_Incidents WHERE Status='Pending'")
+        lblPendingCases.Text = countCases.ToString()
+
+        ' Visual Alert: Turn text RED if there are pending cases
+        If countCases > 0 Then
+            lblPendingCases.ForeColor = Color.Red
+        Else
+            lblPendingCases.ForeColor = Color.Green
+        End If
+    End Sub
+
+    ' NAVIGATION BUTTONS
+    Private Sub btnLogout_Click(sender As Object, e As EventArgs) Handles btnLogout.Click
+        If MessageBox.Show("Are you sure you want to logout?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            Session.CurrentUserID = 0 ' Clear Session
+            Dim login As New frmLogin()
+            login.Show()
+            Me.Close()
+        End If
+    End Sub
+
+    ' Placeholder for Residents Button (We will build this form next)
+    Private Sub btnResidents_Click(sender As Object, e As EventArgs) Handles btnResidents.Click
+        MessageBox.Show("Resident Profiling Module coming next!", "Under Construction", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        ' Dim frm As New frmResidents()
+        ' frm.ShowDialog()
+        ' LoadDashboardStats() ' Refresh stats when returning
+    End Sub
+
+    ' Placeholder for Blotter Button
+    Private Sub btnBlotter_Click(sender As Object, e As EventArgs) Handles btnBlotter.Click
+        MessageBox.Show("Blotter & Incident Module coming next!", "Under Construction", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
 End Class
--- BICTIS/frmLogin.vb
+++ BICTIS/frmLogin.vb
@@ -1,3 +1,44 @@
-﻿Public Class frmLogin
+﻿Imports System.Data.OleDb
 
+Public Class frmLogin
+    ' 1. Create an instance of our Session Engine
+    Dim db As New Session()
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        ' A. Validation
+        If txtUsername.Text = "" Or txtPassword.Text = "" Then
+            MessageBox.Show("Please enter both username and password.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        ' B. Secure Login Query
+        ' We verify the username and password in the database
+        Dim query As String = "SELECT * FROM tbl_Users WHERE Username=@user AND Password=@pass"
+        Dim params As New Dictionary(Of String, Object)
+        params.Add("@user", txtUsername.Text)
+        params.Add("@pass", txtPassword.Text)
+
+        Dim dt As DataTable = db.GetDataTable(query, params)
+
+        If dt.Rows.Count > 0 Then
+            ' C. SUCCESS: Save User Info to Session State
+            Session.CurrentUserID = Convert.ToInt32(dt.Rows(0)("UserID"))
+            Session.CurrentUserRole = dt.Rows(0)("Role").ToString()
+            Session.CurrentUserName = dt.Rows(0)("Username").ToString()
+
+            MessageBox.Show("Welcome, " & Session.CurrentUserName & "!", "Login Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            ' D. Open Dashboard and Close Login
+            Dim dash As New frmDashboard()
+            dash.Show()
+            Me.Hide()
+        Else
+            ' E. FAILURE
+            MessageBox.Show("Invalid Username or Password.", "Access Denied", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Application.Exit()
+    End Sub
 End Class
