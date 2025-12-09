2e9658efc0200b64c7b091bebddc4e0758feca78
Verwin Polloso <verwinpolloso2@gmail.com>
2025-12-09 19:20:06 +0800

    huge changes

--- BICTIS/frmUser.Designer.vb
+++ BICTIS/frmUser.Designer.vb
@@ -1,9 +1,8 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmUser
     Inherits System.Windows.Forms.Form
 
-    'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -14,17 +13,174 @@ Partial Class frmUser
         End Try
     End Sub
 
-    'Required by the Windows Form Designer
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.pnlSidebar = New System.Windows.Forms.Panel()
+        Me.btnHistory = New System.Windows.Forms.Button()
+        Me.btnRequest = New System.Windows.Forms.Button()
+        Me.btnLogout = New System.Windows.Forms.Button()
+        Me.pnlLogo = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.pnlHeader = New System.Windows.Forms.Panel()
+        Me.lblWelcome = New System.Windows.Forms.Label()
+        Me.pnlContainer = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.pnlSidebar.SuspendLayout()
+        Me.pnlLogo.SuspendLayout()
+        Me.pnlHeader.SuspendLayout()
+        Me.pnlContainer.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlSidebar (Resident uses a Green/Teal theme to distinguish from Admin)
+        '
+        Me.pnlSidebar.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(136, Byte), Integer))
+        Me.pnlSidebar.Controls.Add(Me.btnLogout)
+        Me.pnlSidebar.Controls.Add(Me.btnHistory)
+        Me.pnlSidebar.Controls.Add(Me.btnRequest)
+        Me.pnlSidebar.Controls.Add(Me.pnlLogo)
+        Me.pnlSidebar.Dock = System.Windows.Forms.DockStyle.Left
+        Me.pnlSidebar.Location = New System.Drawing.Point(0, 0)
+        Me.pnlSidebar.Name = "pnlSidebar"
+        Me.pnlSidebar.Size = New System.Drawing.Size(220, 600)
+        Me.pnlSidebar.TabIndex = 0
+        '
+        'pnlLogo
+        '
+        Me.pnlLogo.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(121, Byte), Integer), CType(CType(107, Byte), Integer))
+        Me.pnlLogo.Controls.Add(Me.Label1)
+        Me.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlLogo.Location = New System.Drawing.Point(0, 0)
+        Me.pnlLogo.Name = "pnlLogo"
+        Me.pnlLogo.Size = New System.Drawing.Size(220, 80)
+        Me.pnlLogo.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(40, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Text = "RESIDENT"
+        '
+        'btnRequest
+        '
+        Me.btnRequest.Dock = System.Windows.Forms.DockStyle.Top
+        Me.btnRequest.FlatAppearance.BorderSize = 0
+        Me.btnRequest.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnRequest.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.btnRequest.ForeColor = System.Drawing.Color.White
+        Me.btnRequest.Location = New System.Drawing.Point(0, 80)
+        Me.btnRequest.Name = "btnRequest"
+        Me.btnRequest.Padding = New System.Windows.Forms.Padding(15, 0, 0, 0)
+        Me.btnRequest.Size = New System.Drawing.Size(220, 50)
+        Me.btnRequest.TabIndex = 1
+        Me.btnRequest.Text = "Request Clearance"
+        Me.btnRequest.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnRequest.UseVisualStyleBackColor = True
+        '
+        'btnHistory
+        '
+        Me.btnHistory.Dock = System.Windows.Forms.DockStyle.Top
+        Me.btnHistory.FlatAppearance.BorderSize = 0
+        Me.btnHistory.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnHistory.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.btnHistory.ForeColor = System.Drawing.Color.White
+        Me.btnHistory.Location = New System.Drawing.Point(0, 130)
+        Me.btnHistory.Name = "btnHistory"
+        Me.btnHistory.Padding = New System.Windows.Forms.Padding(15, 0, 0, 0)
+        Me.btnHistory.Size = New System.Drawing.Size(220, 50)
+        Me.btnHistory.TabIndex = 2
+        Me.btnHistory.Text = "My History"
+        Me.btnHistory.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnHistory.UseVisualStyleBackColor = True
+        '
+        'btnLogout
+        '
+        Me.btnLogout.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.btnLogout.FlatAppearance.BorderSize = 0
+        Me.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLogout.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.btnLogout.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(205, Byte), Integer), CType(CType(210, Byte), Integer))
+        Me.btnLogout.Location = New System.Drawing.Point(0, 550)
+        Me.btnLogout.Name = "btnLogout"
+        Me.btnLogout.Size = New System.Drawing.Size(220, 50)
+        Me.btnLogout.TabIndex = 5
+        Me.btnLogout.Text = "Sign Out"
+        Me.btnLogout.UseVisualStyleBackColor = True
+        '
+        'pnlHeader
+        '
+        Me.pnlHeader.BackColor = System.Drawing.Color.White
+        Me.pnlHeader.Controls.Add(Me.lblWelcome)
+        Me.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlHeader.Location = New System.Drawing.Point(220, 0)
+        Me.pnlHeader.Name = "pnlHeader"
+        Me.pnlHeader.Size = New System.Drawing.Size(780, 80)
+        Me.pnlHeader.TabIndex = 1
+        '
+        'lblWelcome
+        '
+        Me.lblWelcome.AutoSize = True
+        Me.lblWelcome.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Regular)
+        Me.lblWelcome.Location = New System.Drawing.Point(25, 25)
+        Me.lblWelcome.Name = "lblWelcome"
+        Me.lblWelcome.Size = New System.Drawing.Size(163, 25)
+        Me.lblWelcome.TabIndex = 0
+        Me.lblWelcome.Text = "Welcome Resident"
+        '
+        'pnlContainer
+        '
+        Me.pnlContainer.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.pnlContainer.Controls.Add(Me.Label2)
+        Me.pnlContainer.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlContainer.Location = New System.Drawing.Point(220, 80)
+        Me.pnlContainer.Name = "pnlContainer"
+        Me.pnlContainer.Size = New System.Drawing.Size(780, 520)
+        Me.pnlContainer.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 20.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.Silver
+        Me.Label2.Location = New System.Drawing.Point(200, 200)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Text = "User Services Here"
+        '
+        'frmUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "frmUser"
+        Me.ClientSize = New System.Drawing.Size(1000, 600)
+        Me.Controls.Add(Me.pnlContainer)
+        Me.Controls.Add(Me.pnlHeader)
+        Me.Controls.Add(Me.pnlSidebar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmUser"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "User Dashboard"
+        Me.pnlSidebar.ResumeLayout(False)
+        Me.pnlLogo.ResumeLayout(False)
+        Me.pnlLogo.PerformLayout()
+        Me.pnlHeader.ResumeLayout(False)
+        Me.pnlHeader.PerformLayout()
+        Me.pnlContainer.ResumeLayout(False)
+        Me.pnlContainer.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents pnlSidebar As Panel
+    Friend WithEvents pnlLogo As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnRequest As Button
+    Friend WithEvents btnHistory As Button
+    Friend WithEvents btnLogout As Button
+    Friend WithEvents pnlHeader As Panel
+    Friend WithEvents lblWelcome As Label
+    Friend WithEvents pnlContainer As Panel
+    Friend WithEvents Label2 As Label
 End Class
--- BICTIS/frmUser.vb
+++ BICTIS/frmUser.vb
@@ -1,3 +1,18 @@
 ﻿Public Class frmUser
+    Private Sub frmUser_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblWelcome.Text = "Welcome back, " & Session.CurrentUserName
+    End Sub
 
+    Private Sub btnLogout_Click(sender As Object, e As EventArgs) Handles btnLogout.Click
+        If MessageBox.Show("Log out?", "Confirm", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            Session.CurrentUserID = 0
+            Dim login As New frmLogin()
+            login.Show()
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub btnRequest_Click(sender As Object, e As EventArgs) Handles btnRequest.Click
+        MessageBox.Show("Clearance Request Form coming soon.", "Feature", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
 End Class
